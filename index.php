619bc15f3ed9cb9cb26c22ef3a79e422b21e0105
Alex Fischer <alexfischer57@yahoo.com>
2022-02-08 17:22:42 -0500

    Added model folder and modified controller with hive to store values and display thos values on main page

--- index.php
+++ index.php
@@ -8,12 +8,30 @@ error_reporting(E_ALL);
 
 //Require the autoload file
 require_once('vendor/autoload.php');
+require('model/data-layer.php');
 
 //create an instance of the Base class
 $f3 = Base::instance();
 
 //Define a default route
-$f3->route('GET /',function(){
+$f3->route('GET /',function($f3){
+
+    //Save data to the "hive"
+    $f3->set('username','jshmo');
+    $f3->set('password',sha1('password1'));
+    $f3->set('title',"Working with Template");
+    $f3->set('color','blue');
+
+    $f3->set('fruits',array('apple','orange','banana'));
+
+    $f3->set('desserts',getDesserts());
+
+    $f3->set('colors',getColor());
+
+    //Display list of radio buttons containing colors
+        //Define function that returns array of colors
+        //Add data to hive
+        //Display data in view page.
 
     $view = new Template();
     echo $view->render('views/info.html');
---
+++ model/data-layer.php
@@ -0,0 +1,13 @@
+<?php
+
+function getDesserts()
+{
+    return array('chocolate'=>'Chocolate Mousse',
+        'vanilla'=>'Vanilla Custard',
+        'strawberry'=>'Strawberry Shortcake');
+}
+
+function getColor()
+{
+    return array('blue','red','yellow','green');
+}
